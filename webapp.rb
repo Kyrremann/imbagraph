a12b747af5d34421e14d5401d5a53dbe41386af0
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-02-15 14:52:40 +0100

    Added text and support for several users

--- webapp.rb
+++ webapp.rb
@@ -8,20 +8,29 @@ require_relative 'stats'
 class ImbaGraph < Sinatra::Application
 
   get '/' do
-    items = get_items('kyrremann')
-    unless items
-      return haml(:no_user)
-    end
-
-    yearly_stats = generate_yearly_stats
-    haml :stats, :locals => {
-           "beers" => items.count,
-           "unique_beers" => distinct(items).count,
-           "first_beer" => first_beer(items).to_date,
-           "avg_beers" => avg_beers(items).round(2),
-           "beers_per_yr_day" => beers_per_day(items),
-           "beers_per_day" => beers_per_day(items, yr=false),
-           "yearly_stats" => yearly_stats
-         }
+    get_stats_haml('kyrremann')
   end
+
+  get '/stats/:user' do
+    get_stats_haml(params['user']
+  end
+end
+
+private
+def get_stats_haml(user)
+  items = get_items(user)
+  unless items
+    return haml(:no_user)
+  end
+
+  yearly_stats = generate_yearly_stats
+  haml :stats, :locals => {
+         "beers" => items.count,
+         "unique_beers" => distinct(items).count,
+         "first_beer" => first_beer(items).to_date,
+         "avg_beers" => avg_beers(items).round(2),
+         "beers_per_yr_day" => beers_per_day(items),
+         "beers_per_day" => beers_per_day(items, yr=false),
+         "yearly_stats" => yearly_stats
+       }
 end
