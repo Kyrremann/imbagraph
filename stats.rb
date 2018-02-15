b8ca18bc920747663656cc24250a06386ebc6202
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-02-15 12:52:56 +0100

    Made a simpler version of the app

--- stats.rb
+++ stats.rb
@@ -45,6 +45,37 @@ def most_per_day(items, yr=true)
   beers_per_day(items, yr).order(Sequel.desc(:count)).first[:count]
 end
 
+def first_beer(items)
+  items.min(:created_at)
+end
+
+def avg_beers(items)
+  items.count / (Time.now.to_date - items.min(:created_day)).to_f
+end
+
+def generate_yearly_stats
+  items = get_items
+  years = (items.min(:created_at).year..Time.now.year)
+  data = {}
+
+  years.each do | year |
+    beers = filter_by_year(items, year)
+    data[year.to_s] = {
+      "total" => beers.count,
+      "total_unique" => distinct(beers).count,
+      "max_abv" => max_abv(beers),
+      "avg_abv" => avg_abv(beers).round(2),
+      "countries" => distinct(beers, column_name="brewery_country").count,
+      "breweries" => distinct(beers, column_name="brewery_name").count,
+      "most_per_day" => most_per_day(beers),
+      "most_unique_per_day" => -1
+    }
+  end
+
+  return data
+end
+
+
 private
 def get_column_name(yr)
   (yr ? "created_yr_day" : "created_day").to_sym
--- webapp.rb
+++ webapp.rb
@@ -6,42 +6,22 @@ require 'haml'
 require_relative 'stats'
 
 class ImbaGraph < Sinatra::Application
-  get '/' do
-    haml :index
-  end
 
-  get '/stats/:username/?' do
-    items = get_items(params[:username])
+  get '/' do
+    items = get_items('kyrremann')
     unless items
       return haml(:no_user)
     end
 
+    yearly_stats = generate_yearly_stats
     haml :stats, :locals => {
-           :beers => items.count,
-           :unique_beers => distinct(items).count,
-           :this_year => last_days(items, Time.now.yday).count,
-           :this_month => last_days(items, Time.now.mday).count,
-           :this_week => last_days(items, get_wday).count,
-           :most_per_day => most_per_day(items),
-           :most_unique_per_day => "-1",
-           :avg_abv => avg_abv(items).round(2),
-           :max_abv => max_abv(items),
-           :min_abv => min_abv(items),
-           :countries => distinct(items, column_name="brewery_country").count,
-           :breweries => distinct(items, column_name="brewery_name").count,
-
-           :beers_per_yr_day => beers_per_day(items),
-           :beers_per_day => beers_per_day(items, yr=false)
+           "beers" => items.count,
+           "unique_beers" => distinct(items).count,
+           "first_beer" => first_beer(items).to_date,
+           "avg_beers" => avg_beers(items).round(2),
+           "beers_per_yr_day" => beers_per_day(items),
+           "beers_per_day" => beers_per_day(items, yr=false),
+           "yearly_stats" => yearly_stats
          }
-  end
-
-  def get_wday
-    day = Time.now.wday
-    case day
-    when 0
-      return 7
-    else
-      return day
-    end
   end
 end
