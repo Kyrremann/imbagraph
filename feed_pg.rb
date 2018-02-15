81b0e280b4abb1a23500367975e04dddce42fcb2
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-02-15 14:52:15 +0100

    More like a simple script, supporting file and user

--- feed_pg.rb
+++ feed_pg.rb
@@ -4,36 +4,55 @@ require 'date'
 require 'json'
 require 'sequel'
 
-file = File.read('untappd-05-02-2018.json')
+username = ARGV[0]
+filename = ARGV[1]
+
+unless username and filename
+  p "Missing username or file"
+  p "ruby feed_pg.rb kyrremann untappd-05-02-2018.json"
+  exit(1)
+end
+
+unless File.exist?(filename)
+  p "Can't find #{filename}"
+  exit(1)
+end
+
+file = File.read(filename)
 untappd_json = JSON.parse(file)
 
 database_url = ENV['DATABASE_URL'] ? ENV['DATABASE_URL'] : 'postgres://postgres:postgres@localhost:5432/postgres'
 DB = Sequel.connect(database_url)
 
-DB.create_table :kyrremann do
-  primary_key :id
-  String :beer_name
-  String :beer_type
-  String :brewery_name
-  String :brewery_country
-  Float :beer_abv
-  Integer :beer_ibu
-  Integer :rating_score
-  Date :created_day
-  Date :created_yr_day
-  Time :created_at
+unless DB.table_exists?(username)
+  DB.create_table username do
+    primary_key :id
+    String :beer_name
+    String :beer_type
+    String :brewery_name
+    String :brewery_country
+    Float :beer_abv
+    Integer :beer_ibu
+    Integer :rating_score
+    Date :created_day
+    Date :created_yr_day
+    Time :created_at
+  end
 end
 
-items = DB[:kyrremann]
+items = DB[username.to_sym]
 
+added = 0
 untappd_json.each do | check_in |
+  created_at = check_in['created_at']
+  next if items.where(created_at: created_at).count != 0
+
   beer_name = check_in['beer_name']
   beer_type = check_in['beer_type']
   beer_abv  = check_in['beer_abv'].to_f
   beer_ibu  = check_in['beer_ibu'].to_i
   brewery_name = check_in['brewery_name']
   brewery_country = check_in['brewery_country']
-  created_at = check_in['created_at']
   created_yr_day = DateTime.parse(created_at)
   if created_yr_day.hour < 6
     created_yr_day = created_yr_day - 1
@@ -50,4 +69,7 @@ untappd_json.each do | check_in |
                :created_yr_day => created_yr_day,
                :created_at => created_at,
                :rating_score => rating_score)
+  added = added + 1
 end
+
+p "Added #{added} new rows"
