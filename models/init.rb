a190bbc63aabece2ca2fcd93b793c3494f0ea8fc
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-03-24 12:14:13 +0100

    Trying to fix connection issue with force single connection

--- models/init.rb
+++ models/init.rb
@@ -6,7 +6,6 @@ Sequel.extension :connection_validator
 
 database_url = ENV['DATABASE_URL'] ? ENV['DATABASE_URL'] : 'postgres://postgres:postgres@localhost:5432/imbagraph'
 DB = Sequel.connect(database_url)
-DB.pool.connection_validation_timeout = -1
 
 require_relative 'user'
 require_relative 'beer'
