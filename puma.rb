d286d16d4a55b77d25e642fc936d011367a9f072
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-03-24 12:51:29 +0100

    Close connection when forking

--- puma.rb
+++ puma.rb
@@ -2,8 +2,13 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads(threads_count, threads_count)
 
+before_fork do
+  Sequel::Model.db.disconnect if defined?(Sequel::Model)
+end
+
 preload_app!
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
+
